692475401b0807ae1835b58fcf398c7abebdb403
plzusername <MohamedFaris2210@gmail.com>
2024-05-12 15:36:17 +0300

    Make history recording and processing function

--- knight_travails.js
+++ knight_travails.js
@@ -4,19 +4,18 @@ function adjacentsOfSquare(startingPosition){
         const aboveOrBelowStart = i > 2 && i < 7 ?  -1 : 1
         const leftOfStartOrRight = i % 2 == 0 ? 2 * (i > 4 ?  -1 : 1) : 1 * (i > 4 ?  -1 : 1)
         const rowDiffrenceOneOrTwo = i % 2 == 0 ? 8 : 16
-        relativeSquares.push(startingPosition + aboveOrBelowStart * (leftOfStartOrRight + rowDiffrenceOneOrTwo))
+        relativeSquares.push(knightTemplate(startingPosition + aboveOrBelowStart * (leftOfStartOrRight + rowDiffrenceOneOrTwo)))
     }
 
-    return relativeSquares.filter(node =>  !(node >= 64 || node <= 0  || Math.abs(((startingPosition % 8) || 8) - ((node % 8) || 8)) >= 3))
+    return [knightTemplate(startingPosition) ,relativeSquares.filter(node =>  !(node.pos > 64 || node.pos <= 0  || Math.abs(((startingPosition % 8) || 8) - ((node.pos % 8) || 8)) >= 3))]
 }
 
 const knightChessboardGraph = () => {
     const adjacencyList = []
 
-    for (let i = 0; i < 64; i++) {
+    for (let i = 1; i <= 64; i++) {
         adjacencyList.push(adjacentsOfSquare(i))
     }
-
     return adjacencyList
 }
 
@@ -51,26 +50,46 @@ const knightTemplate = (pos) => {
 
 
 
-function knightMoves(knightPosition, destination){
-    if(knightPosition == destination) return 0
-
+function getKnightMoves(knightPosition, destination){
+    if(knightPosition == destination) return [knightPosition]
     const movesList = queue()
-    movesList.enqueue(knightPosition)
     const knightAdjacencyBoard = knightChessboardGraph()
-    let count = 0
 
-    while(movesList.length != 0){
-        for (let i = 0; i < knightAdjacencyBoard[movesList[0]].length; i++) {
-            movesList.push(knightAdjacencyBoard[movesList[0]][i])
+    const rootKnight = knightTemplate(knightPosition)
+    movesList.enqueue(rootKnight)
 
-            if(knightAdjacencyBoard[movesList[0]][i] == destination){
-                return count || 1
+    const visitedSquares = new Set()
+    visitedSquares.add(rootKnight.pos)
+
+    while(!movesList.isEmpty()){
+        const moves = knightAdjacencyBoard[movesList.queueList[0].pos - 1][1]
+        for (let i = 0; i < moves.length; i++) {
+            const moveInfo = knightAdjacencyBoard[movesList.queueList[0].pos - 1]
+            const moveKnight = moveInfo[0]
+
+            let move = knightAdjacencyBoard[moveKnight.pos - 1]
+            const moveChild = knightAdjacencyBoard[moves[i].pos - 1][0]
+
+            if(visitedSquares.has(moveChild.pos)) continue
+
+            moveChild.addMoveToHistory(move[0])
+
+            movesList.enqueue(moveChild)
+            visitedSquares.add(moveChild.pos)
+            if(moveChild.pos == destination){
+                const history = []
+                let move = moveChild
+                while(move != undefined){
+                    history.unshift(move.pos)
+                    move = move.history[0]
+                }
+
+                return history
             }
         }
-        movesList.shift()
-        count++
-
+        movesList.dequeue()
     }
 }
 
-console.log(knightMoves(1,2))
+const h = getKnightMoves(64,64)
+console.log(h)
